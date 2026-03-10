00ef6ed31a6bfdcf6d312a79ef792bb2d0829b65
Ivan-ObiWan <shum9494@mail.ru>
2026-03-10 19:37:50 +0300

    Add files comparison

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 [![Quality gate](https://sonarcloud.io/api/project_badges/quality_gate?project=Ivan-ObiWan_frontend-project-46)](https://sonarcloud.io/summary/new_code?id=Ivan-ObiWan_frontend-project-46)
 
 [![SonarQube Cloud](https://sonarcloud.io/images/project_badges/sonarcloud-light.svg)](https://sonarcloud.io/summary/new_code?id=Ivan-ObiWan_frontend-project-46)
+
+## Demo 
+
+[![asciicast](https://asciinema.org/a/dQ7DemsZGQ8Begb4.svg)](https://asciinema.org/a/dQ7DemsZGQ8Begb4)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import parseFile from './parsers.js';
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  console.log('File 1:', data1);
-  console.log('File 2:', data2);
-  console.log(`Format: ${format}`);
+  const allKeys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
 
-  return 'Difference will be shown here';
+  const sortedKeys = [...allKeys].sort();
+
+  const lines = sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${value1}`;
+    }
+
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${value2}`;
+    }
+
+    if (value1 !== value2) {
+      return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
+    }
+
+    return `    ${key}: ${value1}`;
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 export default genDiff;
