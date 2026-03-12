97312f819ced6b664674fad85a4f3324d0092e5a
Ivan-ObiWan <shum9494@mail.ru>
2026-03-12 18:57:25 +0300

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -8,23 +8,33 @@ const genDiff = (filepath1, filepath2) => {
   const allKeys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(allKeys);
 
-  const lines = sortedKeys.map((key) => {
+  const lines = sortedKeys.flatMap((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (!_.has(data2, key)) {
+    const hasIn1 = _.has(data1, key);
+    const hasIn2 = _.has(data2, key);
+
+    if (hasIn1 && hasIn2) {
+      if (_.isEqual(value1, value2)) {
+        return `    ${key}: ${value1}`;
+      }
+        return [
+        `  - ${key}: ${value1}`,
+        `  + ${key}: ${value2}`
+      ];
+    }
+
+    if (hasIn1 && !hasIn2) {
       return `  - ${key}: ${value1}`;
     }
-    if (!_.has(data1, key)) {
+
+    if (!hasIn1 && hasIn2) {
       return `  + ${key}: ${value2}`;
     }
-    if (!_.isEqual(value1, value2)) {
-      return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-    }
-    return `    ${key}: ${value1}`;
   });
 
-  return `{\n${lines.join('\n')}\n}`;
+  return `{\n${lines.flat().join('\n')}\n}`;
 };
 
 export default genDiff;
